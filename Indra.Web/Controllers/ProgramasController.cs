92f79981d3516d07c516bb7fd9a816d950b6fc76
John Salas <salas.john@hotmail.com>
2018-08-13 17:50:16 -0500

    SQL Insert

---
+++ Indra.Web/Controllers/ProgramasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Indra.Web.Controllers
+{
+    public class ProgramasController : Controller
+    {
+        // GET: Programas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
